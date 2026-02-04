12cc09848de7afd9fbb3b988560d2355ed9662c7
agarwalsujal <sujalagarwa26@gmai.com>
2026-02-04 13:49:07 +0530

    Add patient data and update API responses

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from pydantic import BaseModel
+import json 
 
-app=FastAPI()
+app = FastAPI()
+
+def load_data():
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data    
 
 @app.get("/")
 def hello():
-    return {"message": "Welcome to the FastAPI application!"} 
+    return {"message": "Patients API"}
+
 
 @app.get("/about")
 def about():
-    return {"message": "hu"}
+    return {"message": "This API provides information about patients."}
+
+@app.get("/view")
+def view_patients():
+    data = load_data()
+    return data
 
